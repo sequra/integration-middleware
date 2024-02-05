76c7fcfc9973e63963be56d6729ab1e596bf56ff
Aleksandar Sasa Boljanovic <aleksandar.boljanovic@logeecom.com>
2024-02-05 16:41:13 +0100

    Fix middleware tests
    ISSUE: SBD-59

--- tests/Unit/Repository/TestQueueItemRepository.php
+++ tests/Unit/Repository/TestQueueItemRepository.php
@@ -4,7 +4,7 @@ namespace SeQura\Middleware\Tests\Unit\Repository;
 
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use SeQura\Middleware\Model\Repository\QueueItemRepository;
+use SeQura\Middleware\ORM\Repositories\QueueItemRepository;
 
 /**
  * Class TestQueueItemRepository
@@ -18,6 +18,13 @@ class TestQueueItemRepository extends QueueItemRepository
      */
     public const TABLE_NAME = 'execution_queue_test';
 
+    private const INDEXED_COLUMNS = [
+        'status' => ['type', 'index_1'],
+        'context' => ['type', 'index_4'],
+        'lastUpdateTimestamp' => ['id', 'type', 'index_7'],
+        'statusQueuePriority' => ['index_1', 'index_3', 'index_8'],
+    ];
+
     /**
      * Creates test entity table.
      */
@@ -29,16 +36,29 @@ class TestQueueItemRepository extends QueueItemRepository
                 static function (Blueprint $table) {
                     $table->increments('id');
                     $table->string('type');
-                    $table->string('index_1')->nullable();
-                    $table->string('index_2')->nullable();
-                    $table->string('index_3')->nullable();
-                    $table->string('index_4')->nullable();
-                    $table->string('index_5')->nullable();
-                    $table->string('index_6')->nullable();
-                    $table->string('index_7')->nullable();
-                    $table->string('index_8')->nullable();
-                    $table->string('index_9')->nullable();
-                    $table->text('data');
+                    for ($i = 1; $i <= 9; $i++) {
+                        $table->string("index_$i")->nullable();
+                    }
+                    $table->bigInteger('parent_id')->nullable();
+                    $table->string('status', 32);
+                    $table->string('context')->nullable();
+                    $table->longText('serialized_task')->nullable();
+                    $table->string('queue_name');
+                    $table->integer('last_execution_progress')->nullable();
+                    $table->integer('progress_base_points')->nullable();
+                    $table->integer('retries')->nullable();
+                    $table->text('failure_description')->nullable();
+                    $table->integer('create_time')->nullable();
+                    $table->integer('start_time')->nullable();
+                    $table->integer('earliest_start_time')->nullable();
+                    $table->integer('queue_time')->nullable();
+                    $table->integer('last_update_time')->nullable();
+                    $table->integer('finish_time')->nullable();
+                    $table->integer('priority')->nullable();
+
+                    foreach (static::INDEXED_COLUMNS as $name => $column) {
+                        $table->index($column, $name);
+                    }
                 }
             );
         }
